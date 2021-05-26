f3a5f3123f7ff94653dc85d333176664b0760dc0
campossrg <campos.srg@gmail.com>
2021-05-26 10:50:15 +0200

    refactor data inside

--- react_props/src/App.js
+++ react_props/src/App.js
@@ -1,12 +1,12 @@
 import Joke from "./Joke"
+import JokesData from "./JokesData"
 
 function App() {
+  const JokesComponent = JokesData.map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />)
+
   return (
     <div className="App">
-      <Joke 
-        question="What's your age?"
-        punchLine="What's a punchLine?"
-      />
+      {JokesComponent}
     </div>
   );
 }
