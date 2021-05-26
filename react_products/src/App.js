b4d27a3f5b2ee4d00835621082a4e898a9ced451
campossrg <campos.srg@gmail.com>
2021-05-26 12:45:57 +0200

    commit classes inside the code

--- react_products/src/App.js
+++ react_products/src/App.js
@@ -1,14 +1,51 @@
 import Product from "./Product"
 import products from "./vschoolProducts"
+import React, {Component} from "react"
+import ReactDOM from "react-dom"
 
-function App() {
-  const productsList = products.map(obj => <Product key={obj.id} name={obj.name} price={obj.price} description={obj.description} />)
+class App extends React.Component {
+  render(){
+    const productsList = products.map(obj => <Product key={obj.id} name={obj.name} price={obj.price} description={obj.description} />)
 
-  return (
-    <div className="App">
-      {productsList}
-    </div>
-  )
+    return(
+      <div className="App">
+        <Header username="Sergio" />
+        <Greeting />
+        {productsList}
+      </div>
+    )
+  }
+}
+
+// #2
+class Header extends React.Component {
+  render() {
+      return (
+          <header>
+              <p>Welcome, {this.props.username}!</p>
+          </header>
+      )    
+  }
+}
+
+// #3
+class Greeting extends Component {
+  render() {
+      const date = new Date()
+      const hours = date.getHours()
+      let timeOfDay
+      
+      if (hours < 12) {
+          timeOfDay = "morning"
+      } else if (hours >= 12 && hours < 17) {
+          timeOfDay = "afternoon"
+      } else {
+          timeOfDay = "night"
+      }
+      return (
+          <h1>Good {timeOfDay} to you, sir or madam!</h1>
+      )
+  }
 }
 
 export default App
